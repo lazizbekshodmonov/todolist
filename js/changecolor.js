3ec2fcec0cf0de1cef0fbcc23d68ca831c4ebb55
Lazizbek Shodmonov <lazizbekshodmonov001@gmail.com>
2022-05-04 13:49:16 +0500

    link firebase database

---
+++ js/changecolor.js
@@ -0,0 +1,43 @@
+let root = document.querySelector(':root');
+
+let changeColorBtn = document.querySelector('.dark__white'),
+    bgImage = document.querySelector('.bg__image img');
+
+
+
+
+let a = true;
+
+
+changeColorBtn.addEventListener('click', () => {
+    if (a) {
+        changeColorBtn.childNodes[1].src = './icons/while.svg';
+        bgImage.src = './image/Bitmap-dark.png';
+
+
+        root.style.setProperty('--todo-item-color', '#25273D');
+        root.style.setProperty('--background', '#171823');
+        root.style.setProperty('--box-shadow', '0px 35px 50px -15px rgba(0, 0, 0, 0.5)');
+        root.style.setProperty('--text-color', '#C8CBE7');
+        root.style.setProperty('--input-color', '#C8CBE7');
+        root.style.setProperty('--completed-text-color', '#4D5067');
+        root.style.setProperty('--border-color', '#393A4B');
+        root.style.setProperty('--hover-color', '#E3E4F1');
+        root.style.setProperty('--footer-text-color', '#5B5E7E');
+        a = false;
+    } else {
+        changeColorBtn.childNodes[1].src = './icons/dark.svg';
+        bgImage.src = './image/Bitmap.png';
+
+        root.style.setProperty('--todo-item-color', '#ffffff');
+        root.style.setProperty('--background', '#F2F2F2');
+        root.style.setProperty('--box-shadow', '0px 35px 50px -15px rgba(194, 195, 214, 0.5)');
+        root.style.setProperty('--text-color', '#494C6B');
+        root.style.setProperty('--input-color', '#393A4B');
+        root.style.setProperty('--completed-text-color', '#D1D2DA');
+        root.style.setProperty('--border-color', '#E3E4F1');
+        root.style.setProperty('--hover-color', '#494C6B');
+        root.style.setProperty('--footer-text-color', '#9495A5');
+        a = true;
+    }
+});
---
+++ js/firebase.module.js
@@ -0,0 +1,67 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+import { getFirestore, doc, getDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdfAbO-lLnl7jf0RYkDy_mJ9X2efAhNIc",
+    authDomain: "todolist-72c88.firebaseapp.com",
+    projectId: "todolist-72c88",
+    storageBucket: "todolist-72c88.appspot.com",
+    messagingSenderId: "628953904303",
+    appId: "1:628953904303:web:a790a5e4aaa4c16325505a",
+    measurementId: "G-C8P3WDSHBF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+let loader = document.querySelector('.loader');
+
+// Get a list of cities from your database
+async function getCities(db) {
+    loader.style.display = 'flex';
+    const citiesCol = collection(db, 'todocontent');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc => doc.data());
+
+    loader.style.display = 'none';
+    console.log(cityList);
+    createDomElement(cityList);
+}
+
+getCities(db);
+
+function createDomElement(data) {
+    let todoContent = document.getElementById('todo__content');
+
+    todoContent.innerHTML = '';
+
+    data.forEach((element) => {
+
+        console.log(element.id);
+        console.log(element.todoitem);
+
+        todoContent.innerHTML += `
+            <li class="todo__item">
+                <label for="id_${element.id}" class="todo__checkbox">
+                    <input type="checkbox" id="id_${element.id}">
+                    <span class="checked__bg">
+                    </span>
+                </label>
+                <p class="todo__text">${element.todotext}</p>
+                <div class="delete">
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M17.6777 0.707107L16.9706 0L8.83883 8.13173L0.707107 0L0 0.707107L8.13173 8.83883L0 16.9706L0.707106 17.6777L8.83883 9.54594L16.9706 17.6777L17.6777 16.9706L9.54594 8.83883L17.6777 0.707107Z" fill="#494C6B"/>
+                        </svg>
+                </div>
+            </li>
+        `;
+
+    });
+}
+
+function firebasePostData(data) {
+
+}
